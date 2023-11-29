35e888dbaf72c0f592a65bcb3f7cccd5d049b3c3
Murilo Baleeiro <murilo.baleeiro@hotmail.com>
2023-11-29 17:00:38 -0300

    FIX: Corrigido script de run do servidor e arquivo de host

--- src/utils/host.ts
+++ src/utils/host.ts
@@ -2,7 +2,7 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 
 const config = {
-  apiBaseUrl: isDevelopment ? "http://localhost:3000" : "http://26.246.216.159:3000",
+  apiBaseUrl: isDevelopment ? "http://localhost:3000" : "http://26.246.216.159:80",
 };
 
 export default config.apiBaseUrl;
